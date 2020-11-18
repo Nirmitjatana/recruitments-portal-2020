bb7aa36631c56e4472c48b5d616794a8c3a6f5f2
SphericalKat <amolele@gmail.com>
2020-11-18 20:02:33 +0530

    chore(antd): install and import
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './App.less'
+import { Button } from 'antd'
 
 function App () {
   return (
@@ -10,6 +11,8 @@ function App () {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type='primary'>Some button</Button>
+        <Button type='primary'>Some button</Button>
         <a
           className='App-link'
           href='https://reactjs.org'
