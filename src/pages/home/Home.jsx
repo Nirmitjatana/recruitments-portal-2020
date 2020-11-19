82e7e8f6d0c31e8af3aea662a58d82843a0e585f
SphericalKat <amolele@gmail.com>
2020-11-19 20:53:10 +0530

    style(headers): change font-family to match design
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,11 +9,11 @@ const Home = (props) => {
     <>
       <Logo />
       <div className='flex flex-col justify-center items-center h-full'>
-        <svg viewBox='0 0 103 16' className='mx-8 md:w-2/6'>
+        <svg viewBox='0 0 98 16' className='mx-8 md:w-2/6'>
           <text x='0' y='15' className='header-title'>Recruitments</text>
         </svg>
-        <svg viewBox='0 0 36 16' className='mx-8 md:w-2/6 text-plasticblue fill-current'>
-          <text x='0' y='15' className='header-title'>2020</text>
+        <svg viewBox='0 0 40 16' className='mx-8 md:w-2/6 text-plasticblue fill-current'>
+          <text x='0' y='15' className='header-year'>2020</text>
         </svg>
         <div className='flex mt-20'>
           <OutlinedButton>Log in</OutlinedButton>
