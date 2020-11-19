19b0f500ea4d31d80d1fbba5a2594f4099cd05fa
SphericalKat <amolele@gmail.com>
2020-11-19 16:09:09 +0530

    refactor(cleanup): re-export config for easier imports
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { routes } from './config/routes'
+import { routes } from './config'
 import './App.css'
 
 function App () {
