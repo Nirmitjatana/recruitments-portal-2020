981251c5ec70c12a58ac7497a942abd0db5f6d03
Your Name <nirmitjatana38@gmail.com>
2020-11-29 19:24:05 +0530

    feat: pages added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Auth from './auth/Auth'
 import Home from './home/Home'
+import tech from './tech-domain/tech'
+import design from './design-domain/design'
+import management from './management-domain/management'
 
-export { Auth, Home }
+export { Auth, Home, tech, design, management }
